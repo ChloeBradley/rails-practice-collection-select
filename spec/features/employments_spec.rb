7f9b7a34d13eca0d3af04ffeba3634698e25ca81
Chloe Bradley <ccbradley77@gmail.com>
2015-03-05 14:46:36 -0700

    created second passing test

--- spec/features/employments_spec.rb
+++ spec/features/employments_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 feature 'employment' do
 
-  scenario 'User can create employment' do
+  scenario 'User select from all people on new employment page' do
+    person = Person.create!(
+      first_name: "Firsty",
+      last_name: "Lasty"
+    )
     visit root_path
     click_link "New Employment"
+    select person.full_name, from: :employment_person_id
+  end
+
+  scenario 'User can select organization' do
+    organization = Organization.create!(
+    name: "gSchool"
+    )
+
+    visit new_employment_path
+    select organization.name, from: :employment_organization_id
   end
 end
