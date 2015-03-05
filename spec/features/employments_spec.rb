75881428a7a663cb45d468d18d8fb51c7fb88906
Chloe Bradley <ccbradley77@gmail.com>
2015-03-05 15:10:55 -0700

    created more working tests

--- spec/features/employments_spec.rb
+++ spec/features/employments_spec.rb
@@ -30,4 +30,45 @@ feature 'employment' do
     click_on "New Employment"
     select job_title.name, from: :employment_job_title_id
   end
+
+  scenario 'User can select a location' do
+    location = Location.create!(
+    name: "1035 Pearl"
+    )
+
+    visit root_path
+    click_on "New Employment"
+    select location.name, from: :employment_location_id
+  end
+
+  scenario "User can select person, organzation, job title and location from new employments page" do
+    person = Person.create!(
+      first_name: "Firsty",
+      last_name: "Lasty"
+    )
+
+    organization = Organization.create!(
+    name: "gSchool"
+    )
+
+    job_title = JobTitle.create!(
+    name: "Manager"
+    )
+
+    location = Location.create!(
+    name: "1035 Pearl"
+    )
+
+    visit root_path
+    click_on "New Employment"
+
+    select person.full_name, from: :employment_person_id
+    select organization.name, from: :employment_organization_id
+    select job_title.name, from: :employment_job_title_id
+    select location.name, from: :employment_location_id
+    click_on "Create Employment"
+
+    expect(current_path).to eq(employments_path)
+
+  end    
 end
